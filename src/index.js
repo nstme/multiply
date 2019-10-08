ec3922a04eb2e9de60fc2029ff4798683237b728
Anastasiia Sierbaieva <51336515+nstme@users.noreply.github.com>
2019-10-07 21:39:53 -0700

    add: task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  let result = (parseInt(first) * parseInt(second)).toString();
+     if (result > Number.MAX_SAFE_INTEGER) {
+    return (BigInt(first) * BigInt(second)).toString();
+    } else {
+        return result;
+    }
 }
